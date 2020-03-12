4dc19f5e5206067b526c36faa2eb681a708ae25a
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-03-13 00:46:28 +0100

    Comments

--- app.js
+++ app.js
@@ -4,42 +4,42 @@ const upload = require('express-fileupload')
 const app = express()
 const port = 3000
 
-app.use(upload())
+
+app.use(upload()) // In order to use file upload module
 
 app.listen(port, () => console.log(`Example app listening on port ` + port + `!: ` + `http://127.0.0.1:3000`))
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html")) //Getting HTML from file
 
-app.post('/', function (req, res) {
-    var filename = req.files.file.name
-    var path = './files/' + filename
-    var search = req.body.text
+app.post('/', function (req, res) { //When posting from this route, from the form
+    var filename = req.files.file.name //Uploaded filename
+    var path = './files/' + filename //Move file to local server path
+    var search = req.body.text //Getting sequence inserted
     
-
-    req.files.file.mv(path, function (err) {
+    req.files.file.mv(path, function (err) { //Moving file to specified local path
           if (err) {
               res.send(err)
           }
           else {
-              readDNA(path, search)
+              readDNA(path, search) //if successfully completed start reading file
           }
       })
 })
 
 function readDNA(path,search) {
-    fs.readFile(path, 'utf8', function (err, contents) {
+    fs.readFile(path, 'utf8', function (err, contents) { //Opening file 
         
-        var indices = [];
-        var idx = contents.indexOf(search);
+        var indices = []
+        var idx = contents.indexOf(search)
         
         while (idx != -1) {
             indices.push(idx);
-            idx = contents.indexOf(search, idx + 1);
+            idx = contents.indexOf(search, idx + 1)
         }
         
-        console.log("Número de ocorrências: " + indices.length);
-        console.log("Indices das ocorrencias: " + indices);
-        var b = indices.map(Number);
+        console.log("Número de ocorrências: " + indices.length)
+        console.log("Indices das ocorrencias: " + indices)
+        var b = indices.map(Number)
         
         var delta = []
         
