d5951f8d557991473aa4af6ac92c080ca38be460
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-03-14 17:22:19 +0100

    Heroku deploy II

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 var fs = require('fs')
 const upload = require('express-fileupload')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 
